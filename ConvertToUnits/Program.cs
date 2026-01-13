36a4cfac64f5ce5c6f0ee246edcea85ea459abd6
kkushnarenko79 <kushnarenko79.gmail.com>
2026-01-13 14:52:30 +0700

    Функция конвертации см в метры

--- ConvertToUnits/Program.cs
+++ ConvertToUnits/Program.cs
@@ -2,24 +2,29 @@
 {
     internal class Program
     {
-        static double ConvertMeterToFeet(float number)
+        static double ConvertMeterToFeet(double number)
         {
             double result = number * 3.28084;
             return result;
         }
 
-        static double ConvertSmToKm(float number)
+        static double ConvertSmToKm(double number)
         {
-            float result = number / 100000;
+            double result = number / 100000;
             return result;
         }
 
-        static double ConvertKmToMeter(float number)
+        static double ConvertKmToMeter(double number)
         {
-            float result = number * 1000;
+            double result = number * 1000;
             return result;
         }
 
+        static double CoonvertSmToMeter(double number)
+        {
+            double result = number * 100;
+            return result;
+        }
         
 
         static void Main(string[] args)
@@ -31,11 +36,13 @@
             Console.WriteLine("4 Сантиметры в метры");
 
             int num = Convert.ToInt32(Console.ReadLine());
-
+            
+            
+            float number = 0;
+            double result = 0;
             while (num != 0)
             {
-                float number = 0;
-                double result = 0;
+                
                 switch (num)
                 {
                     case 1:
@@ -62,6 +69,8 @@
                     case 4:
                         Console.WriteLine("Введите число ");
                         number = Convert.ToInt32(Console.ReadLine());
+                        result = CoonvertSmToMeter(number);
+                        Console.WriteLine(result);
                         break;
 
                     default:
